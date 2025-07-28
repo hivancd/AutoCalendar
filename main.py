f190cb777d98ee05a206590069fa94257916b069
hivancd <hivancdiaz@gmail.com>
2025-07-29 00:32:58 +0200

    added watch_dir = downloads

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import time
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
-
 class MyEventHandler(FileSystemEventHandler):
     def on_created(self, event: FileSystemEvent) -> None:
         print(f"File created: {event.src_path}")
 
-
+WATCH_DIR = "C:/Users/hian/Downloads"  # Directory to watch for file changes
 event_handler = MyEventHandler()
 observer = Observer()
-observer.schedule(event_handler, ".", recursive=True)
+observer.schedule(event_handler, WATCH_DIR, recursive=True)
 observer.start()
 try:
     while True:
