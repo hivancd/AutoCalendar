0173a3a4e9afafbf42f9a37ad96820a79381c7dd
hivancd <hivancdiaz@gmail.com>
2025-08-07 11:00:23 +0200

    added arg to main

--- main.py
+++ main.py
@@ -21,31 +21,53 @@ from watchdog.observers import Observer
 
 from src.autocalendar.pdf_procesor import mark_calendar  # Assuming this is the module where the mark_calendar function is defined
 
+import dotenv
+
+import argparse
+
+parser = argparse.ArgumentParser(description="AutoCalendar PDF Processor")
+parser.add_argument('--user', type=str, help='Windows User to watch Downloads to.', required=True)
+
+user= parser.parse_args().user
+
+print(f"Watching Downloads for user: {user}")
+
+env_path = os.path.join(os.path.dirname(__file__),'.env')
+dotenv.load_dotenv(env_path)  
+# print(env_path)
+api_key = os.getenv('OPENAI_API_KEY')
+
 class MyEventHandler(FileSystemEventHandler):
     def on_created(self, event: FileSystemEvent) -> None:
         try:
             if event.src_path.endswith('.pdf'):  # Check if the created file is a PDF
                 print(f"File created: {event.src_path}")
-                res= mark_calendar(event.src_path)
-        except:
+                res =mark_calendar(event.src_path, api_key)  # Call the mark_calendar function to process the PDF
+                if res:   
+                    print(f"Marked calendar for: {event.src_path}")
+                    print( f"Event details: {res}")
+                else:
+                    print(f"Failed to mark calendar for: {event.src_path}")
+        except Exception as e:
+            print(f"Unexpected error processing file {event.src_path}: {e}")
             return
 
-def main():
+def main(user):
     WATCH_DIR = os.path.join(os.path.expanduser('~'))  # Directory to watch for file changes
-    print(WATCH_DIR)
     downloads_dir = json.load(open('C:\\Windows\\System32\\config\\systemprofile\\address.json'))['address']
     print(downloads_dir)
-    print(os.getlogin())
     event_handler = MyEventHandler()
     observer = Observer()
     observer.schedule(event_handler, downloads_dir, recursive=True)
     observer.start()
+    # f"Watching for new PDF files in {downloads_dir}..."
     try:
         while True:
             time.sleep(1)
     finally:
+        print("Stopping observer...")
         observer.stop()
         observer.join()
 
 if __name__ == "__main__":
-    main()
+    main(user)
--- src/autocalendar/pdf_procesor.py
+++ src/autocalendar/pdf_procesor.py
@@ -2,7 +2,6 @@ from pypdf import PdfReader
 
 import os
 import os.path
-from dotenv import load_dotenv
 from datetime import datetime, date
 
 from pydantic import BaseModel
@@ -15,12 +14,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
-import json
-
-load_dotenv()
-api_key = os.getenv('OPENAI_API_KEY')
-
 
 def read_pdf(file_path):
     """Read a PDF file and return its text content."""
@@ -59,7 +52,7 @@ class Payment(BaseModel):
 SCOPES = ["https://www.googleapis.com/auth/calendar.events.owned"]
 
 
-def mark_calendar(file_path):
+def mark_calendar(file_path, api_key, token_path="token.json"):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -87,24 +80,29 @@ def mark_calendar(file_path):
         output_type=ToolOutput(Payment),
         )
     
-    
-            
         # Run the agent to extract information
+        print(f"Processing file: {file_path}")
         text = read_pdf(file_path)
+        print(f"Extracted text from PDF: {text[:10]}...")  # Print the first 500 characters of the text for debugging
+        # print(agent.capture_run_messages())
+        # print(api_key)
         result = agent.run_sync(text)
+        # print(f"Extracted result: {result.output.document_name}")
         payment = result.output
         # print(payment.payment_to_event())
         
     except HttpError as error:
-        print(f"An error occurred: {error}")
-        return
+        return(f"An error occurred: {error}")
+        
     
     if payment.is_payment is False:
-        print("The document does not refer to a payment.")
-        return
+        return("The document does not refer to a payment.")
+        
     # Next, we mark the date in the calendar:
     # Credentials for the Google Calendar API
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     credentials=os.path.join(script_dir, "credentials.json")
+    print(f"Using credentials from: {credentials}")
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -119,16 +117,21 @@ def mark_calendar(file_path):
         flow = InstalledAppFlow.from_client_secrets_file(
             credentials, SCOPES
         )
-        creds = flow.run_local_server(port=0)
+        creds = flow.run_local_server(host='localhost',
+            port=0, 
+            authorization_prompt_message='Please visit this URL: {url}', 
+            success_message='The auth flow is complete; you may close this window.',
+            open_browser=True
+        )#port=0
       # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())  
-    
+    print("Credentials loaded successfully.")
     # Create the event from the extracted information    
     service = build("calendar", "v3", credentials=creds)
     print(f"Creating event for payment: {payment.payment_reason} on {payment.due_date}")
     event = service.events().insert(calendarId="primary", body=payment.payment_to_event()).execute()
-    print(f"Event created: {event.get('htmlLink')}")
+    return(f"Event created: {event.get('htmlLink')}")
 
 
 if __name__ == "__main__":
